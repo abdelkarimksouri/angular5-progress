fe1fa1c34cf70c35bba0c0622db1b298d6528236
abdelkarim <k.abdelkarim@french-co.fr>
2018-03-08 14:29:56 +0100

    Ajout liste des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecipsComponent } from './recips/recips.component';
+import { RecipeListComponent } from './recips/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recips/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recips/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component' ;
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    HeaderComponent,
+    RecipsComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
